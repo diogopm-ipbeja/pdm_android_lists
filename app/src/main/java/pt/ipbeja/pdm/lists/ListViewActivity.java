c5212ab695df8153c7fef99b88dd6f98849ce802
Diogo Pina Manique <diogo.manique@ubixploit.pt>
2020-10-25 21:27:23 +0000

    Added ListView example

---
+++ app/src/main/java/pt/ipbeja/pdm/lists/ListViewActivity.java
@@ -0,0 +1,81 @@
+package pt.ipbeja.pdm.lists;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Apenas para demonstração da ListView. Não deve usar este tipo de lista mas sim a RecyclerView
+ */
+
+public class ListViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_view);
+
+
+        List<String> dummyData = new ArrayList<>();
+        for (int i = 0; i < 20000; i++) {
+            dummyData.add("Value #"+i);
+        }
+
+        ListView listView = findViewById(R.id.list_view);
+        listView.setAdapter(new MyAdapter(dummyData));
+
+
+    }
+
+    private class MyAdapter extends BaseAdapter {
+
+        private final List<String> data;
+
+        public MyAdapter(List<String> data) {
+            this.data = data;
+        }
+
+        @Override
+        public int getCount() {
+            return data.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return data.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position; // Normalmente utilizar-se-ia um ID mas aqui vou só usar a própria posição
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            if (convertView == null) {
+                convertView = LayoutInflater.from(ListViewActivity.this).
+                        inflate(R.layout.list_view_item, parent, false);
+            }
+
+            // Obter o item para dada posição (sabemos ser uma String)
+            String currentItem = (String) getItem(position);
+
+            // Vamos procurar a TextView dentro do layout
+            TextView tv = convertView.findViewById(R.id.value);
+            // E colocar a String
+            tv.setText(currentItem);
+
+            // Finalmente, devolvemos a View preenchida com a informação
+            return convertView;
+        }
+    }
+}
--- app/src/main/java/pt/ipbeja/pdm/lists/MainActivity.java
+++ app/src/main/java/pt/ipbeja/pdm/lists/MainActivity.java
@@ -17,18 +17,21 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Button scrollViewBtn = findViewById(R.id.scrollview_btn);
+        Button listViewBtn = findViewById(R.id.listview_btn);
         Button recyclerViewBtn = findViewById(R.id.recyclerview_btn);
 
         scrollViewBtn.setOnClickListener(v -> startActivity(new Intent(this, ScrollViewListActivity.class)));
 
+        listViewBtn.setOnClickListener(v -> startActivity(new Intent(this, ListViewActivity.class)));
+
         recyclerViewBtn.setOnClickListener(v -> startActivity(new Intent(this, RecyclerViewListActivity.class)));
 
-        scrollViewBtn.setScaleX(0);
+        /*scrollViewBtn.setScaleX(0);
         scrollViewBtn.setScaleY(0);
         recyclerViewBtn.setScaleX(0);
         recyclerViewBtn.setScaleY(0);
         scrollViewBtn.animate().scaleX(1).scaleY(1).setDuration(500).setStartDelay(100).setInterpolator(new OvershootInterpolator()).start();
-        recyclerViewBtn.animate().scaleX(1).scaleY(1).setDuration(450).setStartDelay(200).setInterpolator(new OvershootInterpolator()).start();
+        recyclerViewBtn.animate().scaleX(1).scaleY(1).setDuration(450).setStartDelay(200).setInterpolator(new OvershootInterpolator()).start();*/
 
     }
 }
